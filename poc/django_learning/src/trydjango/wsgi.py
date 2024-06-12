702802d859862e58a96681b4d085ac5487442a75
fuzail <fuzi98@gmail.com>
2024-06-12 13:08:44 +0530

    fresh learning using youtube course

--- poc/django_learning/src/trydjango/wsgi.py
+++ poc/django_learning/src/trydjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for myBackend project.
+WSGI config for trydjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trydjango.settings')
 
 application = get_wsgi_application()
